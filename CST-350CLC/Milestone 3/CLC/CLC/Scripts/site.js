46da2660e8c26aa2070dd46af49444aabc314a48
rcoon1 <78291341+rcoon1@users.noreply.github.com>
2023-05-30 22:20:18 -0500

    add site.js
    edited shared layout to include site.js

---
+++ CST-350CLC/Milestone 3/CLC/CLC/Scripts/site.js
@@ -0,0 +1,49 @@
+﻿$(function () {
+    blinkeffect('#txtblnk');
+})
+function blinkeffect(selector) {
+    $(selector).fadeOut('slow', function () {
+        $(this).fadeIn('slow', function () {
+            blinkeffect(this);
+        });
+    });
+}
+
+$(function () {
+    console.log("Page is ready");
+
+    $(document).bind("contextmenu", function (e) {
+        e.preventDefault();
+        console.log("Right click. Preventing context menu.");
+    });
+
+    $(document).on("mousedown", "td", function (event) {
+        switch (event.which) {
+            case 1:
+                event.preventDefault();
+
+                var cord = $(this).val();
+                console.log("Cell " + cord + "was left clicked");
+                break;
+            case 3:
+                event.preventDefault();
+                var cord = $(this).val();
+                console.log("Cell " + cord + " was Right clicked");
+                var buttonNumber = $(this).val();
+                if (document.getElementById("cellImage " + buttonNumber).src == "/Content/images/flag.png") {
+                    document.getElementById("cellImage " + buttonNumber).src = "";
+                    console.log("Cell already has an image");
+                }
+                else {
+                    document.getElementById("cellImage " + buttonNumber).src = "/Content/images/flag.png";
+                    console.log("Cell was given an image");
+                }
+
+                break;
+            default:
+                alert('Nothing pressed');
+        }
+    });
+});
+
+              
